26f779e137fe7794601a3b1373250a453a6eba36
Thierry Stofel <137530556+ThierrySTFL@users.noreply.github.com>
2023-11-29 00:17:56 -0300

    menu turma I

---
+++ src/features/turma/menus/menu_opcoes_turma.cpp
@@ -0,0 +1,17 @@
+﻿#include "menu_opcoes_turma.h"
+#include "opcao_menu.h"
+#include "global.h"
+#include "controller_turma.h"
+
+MenuOpcoesTurma::MenuOpcoesTurma(std::string title, ControllerTurma *controllerTurma) : Menu(title), _controllerTurma(controllerTurma)
+{
+  inicializarOpcoes();
+}
+
+void MenuOpcoesTurma::inicializarOpcoes()
+{
+  _opcaoList.push_back(OpcaoMenu("Cadastrar turma", std::bind(&ControllerTurma::cadastraTurma, _controllerTurma)));
+  _opcaoList.push_back(OpcaoMenu("Listar turmas", std::bind(&ControllerTurma::listaTurmas, _controllerTurma)));
+  _opcaoList.push_back(OpcaoMenu("Voltar", std::bind(voltar)));
+}
+
---
+++ src/features/turma/menus/menu_opcoes_turma.h
@@ -0,0 +1,18 @@
+﻿#ifndef INF112_MENU_OPCOES_TURMA_H
+#define INF112_MENU_OPCOES_TURMA_H
+#include "menu.h"
+#include "session.h"
+#include "controller_turma.h"
+
+class MenuOpcoesTurma : public Menu
+{
+protected:
+    Session *_session;
+    ControllerTurma *_controllerTurma;
+    virtual void inicializarOpcoes();
+
+public:
+    MenuOpcoesTurma(std::string title, ControllerTurma *controllerTurma);
+};
+
+#endif

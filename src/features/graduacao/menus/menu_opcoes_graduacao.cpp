696757e1c4869af68c1235c589c1ba8fdd15f24f
Thierry Stofel <137530556+ThierrySTFL@users.noreply.github.com>
2023-11-29 00:07:09 -0300

    menu graduação I

---
+++ src/features/graduacao/menus/menu_opcoes_graduacao.cpp
@@ -0,0 +1,17 @@
+﻿#include "menu_opcoes_graduacao.h"
+#include "opcao_menu.h"
+#include "global.h"
+#include "controller_graduacao.h"
+
+MenuOpcoesGraduacao::MenuOpcoesGraduacao(std::string title, ControllerGraduacao *controllerGraduacao) : Menu(title), _controllerGraduacao(controllerGraduacao)
+{
+  inicializarOpcoes();
+}
+
+void MenuOpcoesGraduacao::inicializarOpcoes()
+{
+  _opcaoList.push_back(OpcaoMenu("Cadastrar graduacao", std::bind(&ControllerGraduacao::cadastraGraduacao, _controllerGraduacao)));
+  _opcaoList.push_back(OpcaoMenu("Listar graduacoes", std::bind(&ControllerGraduacao::listaGraduacoes, _controllerGraduacao)));
+  _opcaoList.push_back(OpcaoMenu("Voltar", std::bind(voltar)));
+}
+
---
+++ src/features/graduacao/menus/menu_opcoes_graduacao.h
@@ -0,0 +1,18 @@
+﻿#ifndef INF112_MENU_OPCOES_GRADUACAO_H
+#define INF112_MENU_OPCOES_GRADUACAO_H
+#include "menu.h"
+#include "session.h"
+#include "controller_graduacao.h"
+
+class MenuOpcoesGraduacao : public Menu
+{
+protected:
+    Session *_session;
+    ControllerGraduacao *_controllerGraduacao;
+    virtual void inicializarOpcoes();
+
+public:
+    MenuOpcoesGraduacao(std::string title, ControllerGraduacao *controllerGraduacao);
+};
+
+#endif

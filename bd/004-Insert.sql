5a4c735a4b575b8a6d07b8ec3c1800f4bd02011e
Lidson Oliveira <lidsonvini2@gmail.com>
2023-12-06 14:19:55 -0300

    Add triggers and insert

---
+++ bd/004-Insert.sql
@@ -0,0 +1,54 @@
+INSERT INTO public."USUARIO"(
+	"NOME", "APELIDO", "SEXO", "LOGIN", "SENHA")
+	VALUES ('Arlindão', 'Bola de Fogo', 'M', 'adm', 'adm',);
+
+INSERT INTO public."MODALIDADE"(
+	"CHAVEMOD", "NOME")
+VALUES (1, 'Jiu Jitsu')
+on conflict do UPDATE set "NOME" = 'Jiu Jitsu';
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Branca', 1, 150);
+	
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Cinza', 2, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Amarela', 3, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Laranja', 4, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Verde', 5, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Azul', 6, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Roxa', 7, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Marrom', 8, 150);
+
+INSERT INTO public."GRADUACAO"(
+	"CHAVEMOD", "NOME", "ORDEM", "MINAULAS")
+	VALUES (1, 'Preta', 9, 150);
+
+INSERT INTO public."USUARIO"(
+	"NOME", "APELIDO", "SEXO", "LOGIN", "SENHA")
+	VALUES ('Lidson', 'Pitbull', 'M', 'lid', '1212');
+
+INSERT INTO public."ALUNO"(
+	"CHAVEUSU", "CHAVEGRD", "NUMAULAS")
+	VALUES (2, 7, 0);
+
+	

d0a4609fceaf4e8b8600ec0f49fcd10214e11fd8
joaovitormelo <joao.galo.2014@gmail.com>
2023-12-12 22:22:23 -0300

    Limpa de arquivos de build

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/thier/OneDrive/Documentos/GitHub/20232-team-5/src
-# Build directory: /mnt/c/Users/thier/OneDrive/Documentos/GitHub/20232-team-5/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
